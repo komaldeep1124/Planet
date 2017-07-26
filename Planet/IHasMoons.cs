51daee0013fbb92ae84ce2fbf113333f29e80d25
komaldeep kaur <KoMaL@LAPTOP-AOK7R0HH>
2017-07-26 17:04:35 -0400

    ihasmoons interface created.

---
+++ Planet/IHasMoons.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planet
+{
+    /// <summary>
+    /// name:komaldeep kaur
+    /// student:300926834
+    /// the ihasmoons interface
+    /// </summary>
+   public  interface IHasMoons
+    {
+        bool HasMoons();
+    }
+}
